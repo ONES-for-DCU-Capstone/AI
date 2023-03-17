79bb3baa67122a11a14d69287beef5526a6bd48f
taemintaeminHyeon <taemin14215@naver.com>
2023-03-17 23:36:54 +0900

    first commit

---
+++ Text Classification/KobertTest.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 15:59:00 2023
+
+@author: hyeontaemin
+"""
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+
+max_len = 64
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 15
+max_grad_norm = 1
+log_interval = 100
+learning_rate =  5e-5
+
+
+device = torch.device("cpu")
+
+bertmodel, vocab = get_pytorch_kobert_model()
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+    
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes=12,
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+
+
+
+model = torch.load("/Users/hyeontaemin/workspace/ENG_AI/sample_databertmodel5.pt",map_location=device)
+
+
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+def predict(predict_sentence):
+
+    data = [predict_sentence, '0']
+    dataset_another = [data]
+
+    another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
+    
+    model.eval()
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length= valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+
+        test_eval=[]
+        for i in out:
+            logits=i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append("채소")
+            elif np.argmax(logits) == 1:
+                test_eval.append("과일")
+            elif np.argmax(logits) == 2:
+                test_eval.append("정육")
+            elif np.argmax(logits) == 3:
+                test_eval.append("빵/떡")
+            elif np.argmax(logits) == 4:
+                test_eval.append("과자/음료/간식")
+            elif np.argmax(logits) == 5:
+                test_eval.append("앙념/오일/조미료/가루/견과류")
+            elif np.argmax(logits) == 6:
+                test_eval.append("유제품/계란")
+            elif np.argmax(logits) == 7:
+                test_eval.append("수산/건어물")
+            elif np.argmax(logits) == 8:
+                test_eval.append("면/통조림/가공식품")
+            elif np.argmax(logits) == 9:
+                test_eval.append("커피/원두/차")
+            elif np.argmax(logits) == 10:
+                test_eval.append("반찬")
+            elif np.argmax(logits) == 11:
+                test_eval.append("담배/주류")
+            
+
+        print(">> 카테고리는 " + test_eval[0] + " 입니다.")
+        
+
+end = 1
+while end == 1 :
+    sentence = input("입력하세요 : ")
+    if sentence == 0 :
+        break
+    predict(sentence)
+    print("\n")
+    
---
+++ Text Classification/MakeModel.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 22:12:47 2023
+
+@author: hyeontaemin
+"""
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+
+max_len = 64
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 15
+max_grad_norm = 1
+log_interval = 100
+learning_rate =  5e-5
+
+
+device = torch.device("cuda:0")
+
+bertmodel, vocab = get_pytorch_kobert_model()
+
+dataset_train = nlp.data.TSVDataset('train.tsv', field_indices=[0,1], num_discard_samples=0)
+dataset_test = nlp.data.TSVDataset('test.tsv', field_indices=[0,1], num_discard_samples=0)
+
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+
+
+#토큰화
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+#BERTDataset 클래스 이용, TensorDataset으로 만들어주기
+data_train = BERTDataset(dataset_train, 0, 1, tok, max_len, True, False)
+data_test = BERTDataset(dataset_test, 0, 1, tok, max_len, True, False)
+
+
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes=12,
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+model = BERTClassifier(bertmodel, dr_rate=0.5).to(device)
+
+
+no_decay = ['bias', 'LayerNorm.weight']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+]
+
+optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+loss_fn = nn.CrossEntropyLoss()
+
+t_total = len(train_dataloader) * num_epochs
+warmup_step = int(t_total * warmup_ratio)
+
+scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
+
+def calc_accuracy(X,Y):
+    max_vals, max_indices = torch.max(X, 1)
+    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
+    return train_acc
+
+
+for e in range(num_epochs):
+    train_acc = 0.0
+    test_acc = 0.0
+    model.train()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(train_dataloader)):
+        optimizer.zero_grad()
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length= valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        loss = loss_fn(out, label)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+        optimizer.step()
+        scheduler.step()  # Update learning rate schedule
+        train_acc += calc_accuracy(out, label)
+        if batch_id % log_interval == 0:
+            print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
+    print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
+    model.eval()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(test_dataloader)):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length= valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        test_acc += calc_accuracy(out, label)
+    print("epoch {} validation acc {}".format(e+1, test_acc / (batch_id+1)))
+    
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+def predict(predict_sentence):
+
+    data = [predict_sentence, '0']
+    dataset_another = [data]
+
+    another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
+    
+    model.eval()
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length= valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+
+        test_eval=[]
+        for i in out:
+            logits=i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append("채소")
+            elif np.argmax(logits) == 1:
+                test_eval.append("과일")
+            elif np.argmax(logits) == 2:
+                test_eval.append("정육")
+            elif np.argmax(logits) == 3:
+                test_eval.append("빵/떡")
+            elif np.argmax(logits) == 4:
+                test_eval.append("과자/음료/간식")
+            elif np.argmax(logits) == 5:
+                test_eval.append("앙념/오일/조미료/가루/견과류")
+            elif np.argmax(logits) == 6:
+                test_eval.append("유제품/계란")
+            elif np.argmax(logits) == 7:
+                test_eval.append("수산/건어물")
+            elif np.argmax(logits) == 8:
+                test_eval.append("면/통조림/가공식품")
+            elif np.argmax(logits) == 9:
+                test_eval.append("커피/원두/차")
+            elif np.argmax(logits) == 10:
+                test_eval.append("반찬")
+            elif np.argmax(logits) == 11:
+                test_eval.append("담배/주류")
+            
+
+        print(">> 카테고리는 " + test_eval[0] + " 입니다.")
+        
+end = 1
+while end == 1 :
+    sentence = input("입력하세요 : ")
+    if sentence == 0 :
+        break
+    predict(sentence)
+    print("\n")
+
+
